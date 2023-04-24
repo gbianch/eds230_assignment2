5625111692d93f354ea6beafdf95ccb939ae3c0e
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-04-24 12:57:48 -0700

    wrote function for min and max yield; checked answers and functiion in Rmd

--- R/almond_modelR.R
+++ R/almond_modelR.R
@@ -6,7 +6,24 @@
 #' @return almond yield (ton acre^-1)
 #'
 # function definition
-almond_modelR = function(temp, precip, year) {
-  yield = (0.015*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28)
-  return(yield)
+almond_modelR = function(clim_data, yr) {
+  data_year <- clim_data %>% 
+    # filter data for year of interest
+    filter(year == yr)
+  
+  feb_temp <- data_year %>% 
+    filter(month == 2) %>% 
+    select(tmin_c)
+  
+  jan_precip <- data_year %>% 
+    filter(month == 1) 
+  
+  jan_total <- sum(jan_precip$precip)
+  
+ # yield = (0.015*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28)
+  
+  max_yield = (-0.015*min(feb_temp)) - (0.0046*min(feb_temp)^2) - (0.07*jan_total) + (0.0043*jan_total^2) + 0.28
+  min_yield = (-0.015*max(feb_temp)) - (0.0046*max(feb_temp)^2) - (0.07*jan_total) + (0.0043*jan_total^2) + 0.28
+ 
+  return(list(max_yield, min_yield))
 }
