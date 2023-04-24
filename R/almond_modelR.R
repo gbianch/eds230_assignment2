9310fe3305542f15598d48f71c58114901094972
Sarah Lam <sdlam@bren.ucsb.edu>
2023-04-24 08:34:28 -0700

    added climate data and made basic structure of function

--- R/almond_modelR.R
+++ R/almond_modelR.R
@@ -1,12 +1,12 @@
 #' Almond Yield Anonomoly Model
 #' 
 #' This function models almond yield anomaly in response to climate
-#' @param T is the minimum temperature in February (C),
-#' @param P is precipitation (mm),
+#' @param temp is the minimum temperature in February (C),
+#' @param precip is total precipitation (mm),
 #' @return almond yield (ton acre^-1)
 #'
 # function definition
-almond_modelR = function(0.015T n,2  0.0046T 2 n;2  0:07P1 + 0.0043P2 1 þ 0:28) {
-  yield = (0.015T n,2  0.0046T 2 n;2  0:07P1 + 0.0043P2 1 þ 0:28)
+almond_modelR = function(temp, precip) {
+  yield = (0.015*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28)
   return(yield)
 }
