e30c40231649641487160b8f8006b8ca3fecefeb
Sarah Lam <sdlam@bren.ucsb.edu>
2023-04-24 09:23:53 -0700

    started sorting data for model

--- R/almond_modelR.R
+++ R/almond_modelR.R
@@ -6,7 +6,7 @@
 #' @return almond yield (ton acre^-1)
 #'
 # function definition
-almond_modelR = function(temp, precip) {
+almond_modelR = function(temp, precip, year) {
   yield = (0.015*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28)
   return(yield)
 }
