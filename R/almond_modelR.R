d4ea436123c4e9117bf45ed36b5f32acaaf07bea
Grace Bianchi <gracebianchi@bren.ucsb.edu>
2023-04-25 12:09:38 -0700

    return dataframe of 3 yield values

--- R/almond_modelR.R
+++ R/almond_modelR.R
@@ -8,25 +8,22 @@
 # function definition
 almond_modelR = function(clim_data, tmincoeff1 = 0.015, tmincoeff2 = 0.0046, pcoeff1 = 0.07, pcoeff2 = 0.0043) {
   
-  clim_data <- clim_data %>% 
-    group_by(month, year) %>% 
+  annual_data <- climate %>% 
+    group_by(year, month) %>% 
     summarize(temp_min = min(tmin_c),
               total_precip = sum(precip))
   
-  feb_temp <- clim_data %>% 
+  feb_temp <- annual_data %>% 
     filter(month == 2) %>% 
-    select(temp_min)
+    select(year, temp_min)
   
-  jan_precip <- clim_data %>% 
-    select(total_precip) %>% 
-    filter(month == 1)
+  jan_precip <- annual_data %>% 
+    filter(month == 1) %>% 
+    select(total_precip, year)
   
  yield = (tmincoeff1*feb_temp$temp_min - tmincoeff2*feb_temp$temp_min^2 - pcoeff1*jan_precip$total_precip + pcoeff2*jan_precip$total_precip^2 + 0.28)
   
- # max_yield = (-0.015*min(feb_temp$temp_min)) - (0.0046*min(feb_temp$temp_min)^2) - (0.07*jan_precip) + (0.0043*jan_precip^2) + 0.28
- # min_yield = (-0.015*max(feb_temp$temp_min)) - (0.0046*max(feb_temp$temp_min)^2) - (0.07*jan_precip) + (0.0043*jan_precip^2) + 0.28
- # mean_yield = (-0.015*mean(feb_temp$temp_min)) - (0.0046*mean(feb_temp$temp_min)^2) - (0.07*jan_precip) + (0.0043*jan_precip^2) + 0.28
  
-  return(list(max(yield), min(yield), mean(yield)))
+  return(data.frame(max_yield = max(yield), min_yield=min(yield), mean_yield=mean(yield)))
 }
 
