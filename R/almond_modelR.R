f96112b115f6406a8feb024be26ed08602e7f59f
Sarah Lam <sdlam@bren.ucsb.edu>
2023-04-24 22:18:50 -0700

    updated function and added picture of conceptual model

--- R/almond_modelR.R
+++ R/almond_modelR.R
@@ -1,4 +1,4 @@
-#' Almond Yield Anonomoly Model
+#' Almond Yield Anomaly Model
 #' 
 #' This function models almond yield anomaly in response to climate
 #' @param clim_data is the numeric data frame of climate data,
@@ -6,25 +6,27 @@
 #' @return almond yield (ton acre^-1)
 #'
 # function definition
-almond_modelR = function(clim_data, yr) {
-  data_year <- clim_data %>% 
-    # filter data for year of interest
-    filter(year == yr)
+almond_modelR = function(clim_data) {
   
-  feb_temp <- data_year %>% 
+  clim_data <- clim_data %>% 
+    group_by(month, year) %>% 
+    summarize(temp_min = min(tmin_c),
+              total_precip = sum(precip))
+  
+  feb_temp <- clim_data %>% 
     filter(month == 2) %>% 
-    select(tmin_c)
+    select(temp_min)
   
-  jan_precip <- data_year %>% 
-    filter(month == 1) 
+  jan_precip <- clim_data %>% 
+                      select(total_precip) %>% 
+                      filter(month == 1)
   
-  jan_total <- sum(jan_precip$precip)
+ yield = (0.015*feb_temp$temp_min - 0.0046*feb_temp$temp_min^2 - 0.07*jan_precip$total_precip + 0.0043*jan_precip$total_precip^2 + 0.28)
   
- # yield = (0.015*temp - 0.0046*temp^2 - 0.07*precip + 0.0043*precip^2 + 0.28)
-  
-  max_yield = (-0.015*min(feb_temp)) - (0.0046*min(feb_temp)^2) - (0.07*jan_total) + (0.0043*jan_total^2) + 0.28
-  min_yield = (-0.015*max(feb_temp)) - (0.0046*max(feb_temp)^2) - (0.07*jan_total) + (0.0043*jan_total^2) + 0.28
-  mean_yield = (-0.015*mean(feb_temp, na.rm = TRUE)) - (0.0046*mean(feb_temp, na.rm = TRUE)^2) - (0.07*jan_total) + (0.0043*jan_total^2) + 0.28
+ # max_yield = (-0.015*min(feb_temp$temp_min)) - (0.0046*min(feb_temp$temp_min)^2) - (0.07*jan_precip) + (0.0043*jan_precip^2) + 0.28
+ # min_yield = (-0.015*max(feb_temp$temp_min)) - (0.0046*max(feb_temp$temp_min)^2) - (0.07*jan_precip) + (0.0043*jan_precip^2) + 0.28
+ # mean_yield = (-0.015*mean(feb_temp$temp_min)) - (0.0046*mean(feb_temp$temp_min)^2) - (0.07*jan_precip) + (0.0043*jan_precip^2) + 0.28
  
-  return(list(max_yield, min_yield, mean_yield))
+  return(list(max(yield), min(yield), mean(yield)))
 }
+
